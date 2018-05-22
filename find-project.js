bbe192a4e4f04096de5f92a2b210de9ea8a264a4
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2018-05-22 13:07:25 +0530

    Add solution for Find Project

---
+++ find-project.js
@@ -0,0 +1,19 @@
+const mongo = require('mongodb').MongoClient;
+
+mongo.connect('mongodb://localhost:27017/', (err, client) => {
+  if (err) throw err;
+  const db = client.db('learnyoumongo');
+  db.collection('parrots').find({
+    age: {
+      $gt: Number(process.argv[2]),
+    },
+  }, {
+    _id: 0,
+    name: 1,
+    age: 1,
+  }).toArray((error, docs) => {
+    if (error) throw error;
+    console.log(docs); // eslint-disable-line
+    client.close();
+  });
+});
